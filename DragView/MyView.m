7f367c1d1969b0b9699a2d0604b7ab395aaab600
wzpziyi1 <1204473090@qq.com>
2015-05-03 15:48:09 +0800

    first commit

---
+++ DragView/MyView.m
@@ -0,0 +1,21 @@
+//
+//  Myview.m
+//  DragView
+//
+//  Created by 王志盼 on 15/5/2.
+//  Copyright (c) 2015年 王志盼. All rights reserved.
+//
+
+#import "Myview.h"
+
+@implementation Myview
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
